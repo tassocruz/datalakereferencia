62b187357cb12e7fc1ad3a9fd5dc7eb82658dcca
Tasso Cruz <tassocruz@icloud.com>
2023-05-08 18:45:59 -0300

    Add files via upload

---
+++ BigData Referência Seguros/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const session = require('express-session');
+
+const port = 3000;
+var path = require('path');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+app.listen(port, () => {
+    console.log("O servidor está rodando.")
+});
